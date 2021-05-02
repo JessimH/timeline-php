337e04067e472013d1c1e867eb06d671a915be4d
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-02 19:34:38 +0200

    ui changes

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -22,8 +22,12 @@
 
     </div>
     <div class="m-4">
-        <a href="" style="display: flex; color: red;"><p style="margin-right: 4px; font-weight: bold; color: black">{{ $post->likes }}</p><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-        </svg></a>
+        <a href="" style="display: flex; color: red;">
+            <p style="margin-right: 4px; font-weight: bold; color: black">{{ $post->likes }}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
+            <p>(ne fonctionne pas)</p>
+        </a>
     </div>
 </div>
